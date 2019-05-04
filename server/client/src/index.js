5c2e736321993b7f7234090dc20d601f7dcd1ba5
Jason Doss <doss2k@gmail.com>
2019-05-04 12:23:30 -0400

    Initial commit for React Frontend and starting redux setup

---
+++ server/client/src/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import promise from "redux-promise";
+
+import App from "./components/App";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import reducers from "./reducers";
+
+// Creates the Redux store
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
+
+const app = (
+  <Provider store={createStoreWithMiddleware(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())}>
+    <App />
+  </Provider>
+);
+
+ReactDOM.render(app, document.getElementById("root"));
