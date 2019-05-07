ae0a38101a76bdf3c366f0322c1ef546d533f1ab
Jason Doss <doss2k@gmail.com>
2019-05-07 18:42:47 -0400

    Make additions to front end along with better styling

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import NCAAPredictor from '../containers/ncaaPredictor';
 
-const App = () => <NCAAPredictor />;
+const App = () => {
+  return (
+    <React.Fragment>
+    <h1 className="text-center" style={{marginBottom: 30, color: 'white'}}>Bracket Un-Buster</h1>
+      <NCAAPredictor />
+      
+    </React.Fragment>
+  
+)};
 
 export default App;
--- server/client/src/containers/ncaaPredictor.js
+++ server/client/src/containers/ncaaPredictor.js
@@ -12,12 +12,29 @@ class NCAAPredictor extends Component {
   }
 
   state = {
-    TeamName: ''
+    team1: '',
+    team2: '',
+    team1logo: '' || 'https://miro.medium.com/max/480/1*0iD-PD-u-9-Kmd_6TakeEw.png',
+    team2logo: '' || 'https://miro.medium.com/max/480/1*0iD-PD-u-9-Kmd_6TakeEw.png',
   }
 
-  //as data is typed, caputer in the state
-  onInputChange = event => {
-    this.setState({ [event.target.name]: event.target.value })
+  
+  onInputChangeTeam1 = (event) => {
+    this.setState({ [event.target.name]: event.target.value });
+    if (this.props.teams[event.target.selectedIndex-1].logoUrl == null) {
+      this.setState({team1logo: 'https://miro.medium.com/max/480/1*0iD-PD-u-9-Kmd_6TakeEw.png'})
+    } else {
+    this.setState({team1logo: this.props.teams[event.target.selectedIndex-1].logoUrl})
+    }
+  }
+
+  onInputChangeTeam2 = (event) => {
+    this.setState({ [event.target.name]: event.target.value });
+    if (this.props.teams[event.target.selectedIndex-1].logoUrl == null) {
+      this.setState({team2logo: 'https://miro.medium.com/max/480/1*0iD-PD-u-9-Kmd_6TakeEw.png'})
+    } else {
+    this.setState({team2logo: this.props.teams[event.target.selectedIndex-1].logoUrl})
+    }
   }
      
   handleSubmit = (e) => {
@@ -29,35 +46,57 @@ class NCAAPredictor extends Component {
     if(!this.props.teams) {
       return('Loading....')
     }
+
     return (
-
-     // Creates drop down menu that populates with all of the available teams
-
-      <div className="container">
-        <h1 className="text-center" style={{marginBottom: 30, color: 'white'}}>Bracket Un-Buster</h1>
+      <div className="container">        
         <div className="row justify-content-center">
-          <form onSubmit={this.handleSubmit}>
-            <select
-              className="form-control teamSelect"
-              value={this.state.TeamName}
-              onChange={this.onInputChange}
-              name="TeamName"
-            >
-              <option value="default">Select Team</option>
-              {this.props.teams.map(teams => (
-                <option
-                  key={teams.TeamName}
-                  value={teams.TeamName}
-                  name={teams.TeamName}
+            <div className="col-md-4">
+              <select
+                className="form-control teamSelect"
+                value={this.state.team1}
+                onChange={this.onInputChangeTeam1}
+                name="team1"
+                style={{marginTop: 50}}
+              >
+                <option value="default" style={{display: 'none'}}>Select Team 1</option>
+                {this.props.teams.map(teams => (
+                  <option
+                    key={teams.TeamName}
+                    value={teams.TeamName}
+                    name={teams.TeamName}
+                  >
+                    {teams.TeamName}
+                  </option>
+                ))}
+              </select>
+              <img style={{marginTop: 20}} className="teamLogo img-fluid" src={this.state.team1logo} />
+            </div>
+            <div className="col-md-4 text-center">            
+              <button className="btn btn-primary btn-lg" type="submit" style={{marginTop: 250}} disabled={this.state.team1 === this.state.team2}>
+                Predict
+              </button>
+            </div>
+            <div className="col-md-4">
+              <select
+                  className="form-control teamSelect"
+                  value={this.state.team2}
+                  onChange={this.onInputChangeTeam2}
+                  name="team2"
+                  style={{marginTop: 50}}
                 >
-                  {teams.TeamName}
-                </option>
-              ))}
-            </select>            
-            <button className="btn btn-primary btn-sm" type="submit" style={{marginLeft: 10}}>
-              Predict
-            </button>
-          </form>
+                  <option value="default" style={{display: 'none'}}>Select Team 2</option>
+                  {this.props.teams.map(teams => (
+                    <option
+                      key={teams.TeamName}
+                      value={teams.TeamName}
+                      name={teams.TeamName}
+                    >
+                      {teams.TeamName}
+                    </option>
+                  ))}
+                </select>
+                <img style={{marginTop: 20}} className="teamLogo img-fluid" src={this.state.team2logo} />
+            </div>
         </div>
       </div>        
     );
